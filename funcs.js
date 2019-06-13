6e975d90a2a8c285d6e555ef23a1c861df1056ab
nathanielltaylor <ntaylor390@gmail.com>
2019-06-13 16:38:19 -0500

    4 decimal inputs, correct classification, causation buttons, causation functionality unimplemented

---
+++ funcs.js
@@ -0,0 +1,222 @@
+var finalOutputs = [];
+var effects = [];
+
+function findActualCause() {
+  alert(effects);
+}
+
+function mousePressed() {
+  var d1, d2; 
+  d1 = dist(mouseX, mouseY, 640, 320);
+  d2 = dist(mouseX, mouseY, 640, 220);
+  fill(color(100, 50, 150));
+  if (d1 < 16) {
+    network.nodes[12].selected = !network.nodes[12].selected;
+    if (network.nodes[12].selected) {
+      effects.push('Output node 2');
+    } else {
+      effects = effects.filter(w => w != 'Output node 2');
+    }
+  } else if (d2 < 16) {
+    network.nodes[11].selected = !network.nodes[11].selected;
+    if (network.nodes[11].selected) {
+      effects.unshift('Output node 1');
+    } else {
+      effects = effects.filter(w => w != 'Output node 1');
+    }
+  }
+  causationBody.html(effects.join(' '));
+  if (effects.length > 0) {
+    causationButton.show();
+  } else {
+    causationButton.hide();
+  }
+}
+
+function run() {
+  result.html('');
+  finalOutputs = [];
+  const vector = input.value();
+  title.html('Input: ' + vector);
+  input.value('');
+  v = vector.split('');
+  for (i=0;i<4;i++) {
+    v[i] = int(v[i]);
+  }
+  network.feedforward(v[0], v[1], v[2], v[3]);
+}
+
+function connectLayers(layer1, layer2, weights) {
+    for (i=0;i<layer1.length;i++) {
+        for (j=0;j<layer2.length;j++) {
+         network.connect(layer1[i], layer2[j], weights[i][j]);
+        }
+    }
+}
+
+function printLabels() {
+  var cl, clString;
+  cl = str(Math.round(finalOutputs[0])) + str(Math.round(finalOutputs[1]));
+  if (cl == '00') {
+    clString = 'Iris setosa';
+  } else if (cl == '01') {
+    clString = 'Iris versicolor';
+  } else if (cl == '10') {
+    clString = 'Iris virginica';
+  } else {
+    clString = 'Not a known species';
+  }
+  result.html('Classification: ' + cl + ', ' + clString);
+}
+
+function Connection(from, to, w) {
+    this.a = from;
+    this.b = to;
+    this.weight = w;
+    this.sending = false;
+    this.sender = null;
+    this.output = 0;
+    
+    
+    this.feedforward = function(val) {
+      this.output = val*this.weight;
+      this.sender = this.a.position.copy();
+      console.log(this.output);
+      // this.sender.r = Math.abs(this.output*75);
+      // this.sender.r = 64;
+      this.sending = true;
+    }
+    
+    this.update = function() {
+      if (this.sending) {
+        this.sender.x = lerp(this.sender.x, this.b.position.x, 0.1);
+        this.sender.y = lerp(this.sender.y, this.b.position.y, 0.1);
+        var d = p5.Vector.dist(this.sender, this.b.position);
+        if (d < 1) {
+          this.b.feedforward(this.output);
+          this.sending = false;
+        }
+      }
+    }
+    
+    this.display = function() {
+      stroke(0);
+      strokeWeight(this.weight);
+      line(this.a.position.x, this.a.position.y, this.b.position.x, this.b.position.y);
+      
+      if (this.sending) {
+        fill(0);
+        ellipse(this.sender.x, this.sender.y, 16, 16);
+      }
+    }
+  }
+  
+  function Network(x, y) { 
+    this.nodes = [];
+    this.connections = [];
+    this.position = createVector(x, y);
+    
+    this.addNode = function(n) {
+      this.nodes.push(n);
+    }
+    
+    this.connect = function(a, b, weight) {
+      var c = new Connection(a, b, weight);
+      a.addOutConnection(c);
+      b.addInConnection(c);
+      this.connections.push(c);
+    }
+    
+    this.feedforward = function() {
+      for (var i = 0; i < arguments.length; i++) {
+          var n = this.nodes[i];
+          n.feedforward(arguments[i], 1);
+      }
+    }
+    
+    this.update = function() {
+      for (var i = 0; i < this.connections.length; i++) {
+        this.connections[i].update();
+      }
+    }
+    
+    this.display = function() {
+      push();
+      translate(this.position.x, this.position.y);
+      for (var i = 0; i < this.nodes.length; i++) {
+        this.nodes[i].display();
+      }
+      
+      for (var i = 0; i < this.connections.length; i++) {
+        this.connections[i].display();
+      }
+      pop();
+    }
+  }
+  
+  function Node(x, y, func) { 
+    this.position = createVector(x, y);
+    this.in_connections = [];
+    this.out_connections = [];
+    this.inputs_received = 0;
+    this.sum = 0;
+    this.r = 32;
+    this.func = func;
+    this.selected = false;
+    
+    this.addInConnection = function(c) {
+      this.in_connections.push(c);
+    }
+
+    this.addOutConnection = function(c) {
+      this.out_connections.push(c);
+    }
+    
+    this.feedforward = function(input) {
+      this.inputs_received += 1;
+      this.sum += input;
+      if (this.in_connections.length == this.inputs_received || this.func == 'input') {
+        this.fire();
+        this.inputs_received = 0;
+        this.sum = 0;
+      }
+    }
+    
+    this.fire = function() {
+      var out;
+      if (this.func == 'tanh') {
+        out = Math.tanh(this.sum);
+      } else if (this.func == 'sigmoid') {
+        out = 1/(1+Math.E**(this.sum*-1));
+      } else {
+        out = this.sum;
+      }
+      if (this.out_connections.length != 0) {
+        for (var i = 0; i < this.out_connections.length; i++) {
+          this.out_connections[i].feedforward(out);
+        }
+      } else {
+        finalOutputs.push(out);
+        if (finalOutputs.length > 1) {
+          printLabels();
+        }
+      }
+      //Currently rendering circle pulse in proportion to signal
+      //not sure if I like this
+      this.r = 64 * Math.abs(out);
+    }
+    
+    this.display = function() {
+      stroke(0);
+      strokeWeight(1);
+      var b = map(this.sum,0,1,255,0);
+      fill(b);
+      if (this.selected == true) {
+        fill('red');
+      }
+      ellipse(this.position.x, this.position.y, this.r, this.r);
+      
+      this.r = lerp(this.r,32,0.1);
+    }
+  }
+
---
+++ sketch.js
@@ -0,0 +1,95 @@
+var network;
+let il, hl1, hl2, ol, weights1, weights2, weights3, input, button, title, result;
+
+weights1 = [
+  [-0.2811654 , -0.36876372, -0.19776508, -0.31375873],
+  [-1.148887  , -0.5479878 , -1.1339006 ,  0.16764727],
+  [ 1.3770278 ,  0.53476024, -1.0114052 , -0.17715394],
+  [ 1.8477372 ,  1.0137312 , -0.02374263,  0.9401175 ]
+]
+
+weights2 = [
+  [ 0.8035842 , -1.4213372 , -0.7694416 ],
+  [ 0.6442775 ,  1.2488205 ,  1.4130265 ],
+  [ 1.0548642 , -0.5402939 ,  0.23218893],
+  [-0.02070524, -0.73209924, -0.75683004]
+]
+
+weights3 = [
+  [ 1.9256688 ,  3.0619571 ],
+  [-2.9531674 , -0.09858043],
+  [-3.1945324 ,  3.0199397 ]
+]
+
+function setup() { 
+  createCanvas(1280, 640);
+
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('Submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(run);
+
+  title = createElement('h2', 'Enter a 4 digit number');
+  title.position(20, 5);
+
+  result = createElement('h2', '');
+  result.position(20, 600);
+
+  causationTitle = createElement('h2', 'Actual Causation');
+  causationTitle.position(850, 65);
+
+  causationBody = createElement('p', '');
+  causationBody.position(850, 100);
+
+  causationButton = createButton('Find Causes of Selected Nodes');
+  causationButton.position(850, 200);
+  causationButton.mousePressed(findActualCause);
+  causationButton.hide();
+
+  textAlign(CENTER);
+  textSize(50);
+
+  network = new Network(width/2, height/2);
+  
+  il = [
+    new Node(-600, -200, 'input'),
+    new Node(-600, -100, 'input'),
+    new Node(-600, 0, 'input'),
+    new Node(-600, 100, 'input')
+  ]
+  
+  hl1 = [
+    new Node(-400, -200, 'tanh'),
+    new Node(-400, -100, 'tanh'),
+    new Node(-400, 0, 'tanh'),
+    new Node(-400, 100, 'tanh')
+  ]
+
+  hl2 = [
+      new Node(-200, -125, 'tanh'),
+      new Node(-200, -50, 'tanh'),
+      new Node(-200, 25, 'tanh')
+  ]
+
+  ol = [
+    new Node(0, -100, 'sigmoid'),
+    new Node(0, 0, 'sigmoid')
+  ]
+  
+  connectLayers(il, hl1, weights1);
+  connectLayers(hl1, hl2, weights2);
+  connectLayers(hl2, ol, weights3);
+
+  allNodes = il.concat(hl1, hl2, ol);
+  for (i=0;i<allNodes.length;i++) {
+      network.addNode(allNodes[i]);
+  }
+} 
+
+function draw() { 
+  background(255);
+  network.update();
+  network.display();
+}
